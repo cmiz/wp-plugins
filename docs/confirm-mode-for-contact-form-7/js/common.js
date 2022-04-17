59695441c0e5056f5c04a098f9068de9dc37c5bd
cmiz <stty1975@gmail.com>
2022-04-17 23:51:46 +0900

    docs

---
+++ docs/confirm-mode-for-contact-form-7/js/common.js
@@ -0,0 +1,49 @@
+(() => {
+
+    function nav(html) {
+        if (html) nav.html = html;
+        if (!nav.html) return;
+        if (!nav.sidebar) {
+            const sidebar = document.querySelector('body > aside');
+            if (sidebar) {
+                sidebar.insertAdjacentHTML('beforeend', nav.html);
+                nav.sidebar = true;
+            }
+        }
+        if (!nav.footer) {
+            const footer = document.querySelector('body > footer');
+            if (footer) {
+                footer.insertAdjacentHTML('afterbegin', nav.html);
+                nav.footer = true;
+            }
+        }
+    }
+    nav.html = '';
+    nav.footer = false;
+    nav.sidebar = false;
+
+    fetch('parts/header.html')
+        .then(resp => resp.text())
+        .then(html => {
+            document.body.insertAdjacentHTML('beforeend', html);
+        });
+
+    fetch('parts/footer.html')
+        .then(resp => resp.text())
+        .then(html => {
+            document.body.insertAdjacentHTML('beforeend', html);
+            nav();
+        });
+
+    fetch('parts/sidebar.html')
+        .then(resp => resp.text())
+        .then(html => {
+            document.body.insertAdjacentHTML('beforeend', html);
+            nav();
+        });
+
+    fetch('parts/nav.html')
+        .then(resp => resp.text())
+        .then(html => nav(html));
+
+})();
